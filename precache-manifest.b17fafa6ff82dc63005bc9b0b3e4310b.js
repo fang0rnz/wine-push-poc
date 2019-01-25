b710f6beb18bf874a0d000bb144e99590dd2e442
Lucas Sartori <lucas.sartm@gmail.com>
2019-01-25 16:20:28 -0200

    Updates

---
+++ precache-manifest.b17fafa6ff82dc63005bc9b0b3e4310b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wine-push-poc/robots.txt"
+  },
+  {
+    "revision": "72c158f7c45cd4fbcb44",
+    "url": "/wine-push-poc/js/chunk-vendors.256e0553.js"
+  },
+  {
+    "revision": "2c163ebc2a10ca0175ea",
+    "url": "/wine-push-poc/js/app.5c50cecb.js"
+  },
+  {
+    "revision": "89f61c7c31dfe73034e37dee56dbd099",
+    "url": "/wine-push-poc/index.html"
+  },
+  {
+    "revision": "2c163ebc2a10ca0175ea",
+    "url": "/wine-push-poc/css/app.66b35d51.css"
+  }
+];
